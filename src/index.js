2cd8f414505e8a067192cf0d5b96c25da5d6b542
Kevin <qeven03@gmail.com>
2024-01-08 14:16:53 -0600

    restaurant page

--- src/index.js
+++ src/index.js
@@ -62,12 +62,10 @@ function reveal() {
     for (let i = 0; i < reveals.length; i++) {
       let windowHeight = window.innerHeight;
       let elementTop = reveals[i].getBoundingClientRect().top;
-      let elementVisible = 250;
+      let elementVisible = 300;
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+      } 
     }
   }
